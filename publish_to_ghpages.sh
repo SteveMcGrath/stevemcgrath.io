7940b947f66d2ba18bd21a75b942282f67ca7cac
Steven McGrath <steve@chigeek.com>
2017-07-18 12:05:33 -0500

    Updates config file and changed publishing script

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -23,5 +23,12 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Updating gh-pages branch and pushing"
+cd public
+git add --all
+git commit -m "Publishing to gh-pages (publish.sh)"
+git push
+
+echo "Pushing any remaining commits to the master branch"
+cd ..
+git push
