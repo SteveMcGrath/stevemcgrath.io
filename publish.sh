165db6c39387028098047248e5646225b3b75386
Steven McGrath <steve@chigeek.com>
2019-01-05 00:20:58 -0600

    renamed publishing script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-cd $(dirname "$0")
+DIR=$(dirname "$0")
 
-if [[ $(git status -s) ]]
-then
+#cd ${DIR}/..
+
+if [[ $(git status -s) ]];then
     echo "The working directory is dirty. Please commit any pending changes."
     exit 1;
 fi
@@ -15,7 +16,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+git worktree add -fB gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -23,12 +24,5 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch and pushing"
-cd public
-git add --all
-git commit -m "Publishing to gh-pages (publish.sh)"
-git push
-
-echo "Pushing any remaining commits to the master branch"
-cd ..
-git push
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
